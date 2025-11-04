0226e6647d8b52178bc34487da3fd904b87278cd
Mark Meyer <12794995+phytrisha@users.noreply.github.com>
2025-11-04 11:55:56 +0100

    added image to project description

--- data/projects/concept-learning.ts
+++ data/projects/concept-learning.ts
@@ -27,6 +27,11 @@ export const conceptLearning = {
     'The following will analyze and substantiate these premises and explore their implications with respect to concept learning under conditions of ideology. I will introduce and draw on Sally Haslanger’s account of ideology and Lisa Feldman Barrett’s work on how a predictive brain constructs mental phenomena, before pointing out what I consider to be interesting conceptual intersections, and their implications with respect to the subject of interest: dismantling ideologies. The empirical evidence and neuroscientific theories referred to will be taken as axiomatic presuppositions, though their truth value might change in the light of future discoveries and may even be controversial given the current state of the field. There will be no deductive proof of the intersections I seek, and what seems valid will only become conclusive in approximation by abduction. The goal is as much to point out interesting conceptual ramifications, as it is asking questions and identifying potential challenges, most of which will remain unaddressed. In short: I will proceed by linking an account of ideology to an account of the human brain as a generator of situated conceptualizations, and explore the implications of that intersection with respect to concept learning.',
      ],
     },
+    { 
+      type: 'image', 
+      src: '/images/project-screenshot.jpg',
+      alt: 'Project screenshot'
+    },
   ] as DescriptionBlock[],
 
   contentSections: [
