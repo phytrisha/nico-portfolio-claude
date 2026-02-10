225bfc8b8d0c45a3fc8e49209fd302a2bdd2b26b
ncgk <41481154+ncgk@users.noreply.github.com>
2026-02-10 12:44:53 +0100

    10.02.2

--- data/projects/collection.ts
+++ data/projects/collection.ts
@@ -5,7 +5,7 @@ export const collection: Project = {
   slug: 'collection',
   title: 'A couple more things...',
   shortTitle: 'A couple more things...',
-  category: 'freelance',
+  category: 'client work',
   tags: ['INTERACTION DESIGN & FREELANCE WORK'],
   year: '2014 – 2025',
   color: '#E6E2D8',
